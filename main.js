f028ba04b6a4d5271599fee529a03c69c652d9e3
Dylan Sieren <dylan.sieren01@gmail.com>
2018-12-23 11:11:29 -0600

    set up list button, added some animations for the list. Still want to work on an animation firing off when the list is closed

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // full list buton. Toggle classes that allow the button to appear with a fade in, then toggle it off to go back to display
 //none.
+
+
+const seeMoreServicesButton = document.getElementById('full-list-button');
+const fullListOfServices = document.getElementById('list-of-services');
+
+
+seeMoreServicesButton.addEventListener('click', showList);
+
+function showList() {
+    this.classList.toggle("list-active");
+    fullListOfServices.classList.toggle("show");
+    fullListOfServices.classList.toggle("fadeInUp");
+}
